2e6ca9747ada8c9c3d5913c811a09bdf3269ce79
yewon <ipunwon_@naver.com>
2022-11-22 22:08:32 +0900

    2022년 11월 22일

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,12 +1,18 @@
 <template>
-
-  <Modal :원룸들="원룸들" :누른거="누른거" :모달창열렸니="모달창열렸니"></Modal>
+  <transition name="fade">
+    <Modal @closeModal="모달창열렸니 = false" :원룸들="원룸들" :누른거="누른거" :모달창열렸니="모달창열렸니"></Modal>
+  </transition>
 
   <div class="menu">
     <a v-for="(a,i) in 메뉴들" :key="i">{{ a }}</a>
   </div>
+  
+  <p>지금 결제하면 {{ amount }}% 할인</p>
+  <Discount v-if="showDiscount == true"></Discount>
 
-  <Discount></Discount>
+  <button @click="priceSort">가격순정렬</button>
+  <button @click="sortBack">되돌리기</button>
+
 
   <Card @openModal="모달창열렸니 = true; 누른거=$event" :원룸="원룸들[i]" v-for="(작명,i) in 원룸들" :key="i"></Card>
 
@@ -23,6 +29,9 @@ export default {
   name: 'App',
   data(){
     return{
+      amount : 30,
+      showDiscount : true,
+      원룸들오리지널 : [...data],
       누른거 : 0,
       원룸들 : data,
       모달창열렸니 : false,
@@ -34,7 +43,23 @@ export default {
   methods: {
     increase(){
       this.신고수 += 1;
+    },
+    sortBack(){
+      this.원룸들 = [...this.원룸들오리지널];
+    },
+    priceSort(){
+      this.원룸들.sort(function(a,b){
+        return a.price - b.price
+      })
     }
+  },
+  mounted(){
+    setTimeout(()=>{
+      this.showDiscount = false;
+    }, 2000),
+    setInterval(()=>{
+      this.amount--;
+    }, 1000);
   },
   components: {
     Discount,
@@ -45,6 +70,18 @@ export default {
 </script>
 
 <style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 1s;
+}
+
+.fade-enter-to {
+  opacity: 1;
+}
+
 .discount {
   background: #eee;
   padding: 10px;
--- vuedongsan/src/ModalView.vue
+++ vuedongsan/src/ModalView.vue
@@ -4,8 +4,9 @@
       <img :src="원룸들[누른거].image" style="width:100%">
       <h4>{{ 원룸들[누른거].title }}</h4>
       <p>{{ 원룸들[누른거].content }}</p>
-      <p>{{ 원룸들[누른거].price }}</p>
-      <!-- <button @click="모달창열렸니 = false">닫기</button> -->
+      <input v-model="month">
+      <p> {{ month }}개월 선택함 : {{ 원룸들[누른거].price * month }}</p>
+      <button @click="$emit('closeModal')">닫기</button>
     </div>
   </div>
 </template>
@@ -13,6 +14,24 @@
 <script>
 export default {
     name: 'ModalView',
+    data(){
+      return {
+        month : 1,
+      }
+    },
+    beforeUpdate(){
+      if(this.month == 2){
+        alert("2개월은 너무 적음..안팝니다")
+      }
+    },
+    watch: {
+      month(a){
+        if(isNaN(a) == true){
+          alert('문자입력하지마라')
+          this.month = 1;
+        }
+      }
+    },
     props: {
         원룸들 : Array,
         누른거 : Number,
